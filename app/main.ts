b2746bd155c53785763fe37f5f09f1bd41001baf
Irvin <96852240+irvingm45@users.noreply.github.com>
2026-06-09 13:13:39 -0600

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -3,11 +3,21 @@ import * as net from "net";
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// TODO: Uncomment the code below to pass the first stage
+const okResponse = "HTTP/1.1 200 OK\r\n\r\n";
 const server = net.createServer((socket) => {
+  console.log("User connected");
+
+  // When user send information through the socket
+  socket.on("data", (data) => {
+    socket.write(okResponse);
+  });
+
+  // This event listens
   socket.on("close", () => {
-    socket.end();
+    console.log("User disconnected");
   });
 });
 
-server.listen(4221, "localhost");
+server.listen(4221, "localhost", () => {
+  console.log("Server listening on 4221");
+});
